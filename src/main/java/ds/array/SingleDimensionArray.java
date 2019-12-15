d5289053427d2a8967b544613ab3b218e9f9b0ef
EXTMASZL <deshmukh259.2010@gmail.com>
2019-12-15 19:55:31 +0000

    array

---
+++ src/main/java/ds/array/SingleDimensionArray.java
@@ -0,0 +1,48 @@
+package ds.array;
+
+public class SingleDimensionArray {
+	int arr[] = null;
+
+    
+    //Constructor
+	public SingleDimensionArray(int sizeOfArray) {
+	
+	}
+
+	
+    // Print the array
+	public void traverseArray() {
+	
+		
+	}
+    
+    
+	//Insert value in the Array
+	public void insert(int location, int valueToBeInserted) {
+	
+	}
+
+	
+	
+
+		
+	// Access a particular element of an array
+	public void accessingCell(int cellNumber) {
+		}
+
+	
+	//Search for an element in the given Array
+	public void searchInAnArray(int valueToSearch) {
+	}
+
+
+	//Delete value from given Array
+	public void deleteValueFromArray(int deleteValueFromThisCell) {
+	}
+	
+	
+	//Delete the entire Array
+	public void deleteThisArray() {
+	}
+	
+}//end of class
