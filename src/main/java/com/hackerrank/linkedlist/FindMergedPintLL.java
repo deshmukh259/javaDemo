7e2b42e83a11ee6df0b822ec2c6ca5255fb35c49
EXTMASZL <deshmukh259.2010@gmail.com>
2019-12-26 17:52:28 +0000

    ll

---
+++ src/main/java/com/hackerrank/linkedlist/FindMergedPintLL.java
@@ -0,0 +1,30 @@
+package com.hackerrank.linkedlist;
+
+public class FindMergedPintLL {
+
+
+    static class
+    SinglyLinkedListNode {
+        int data;
+        SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int data) {
+            this.data = data;
+        }
+    }
+// *****
+    static int findMergNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        if (head1 == null || head2 == null) return 0;
+        while (head1 != null) {
+            SinglyLinkedListNode head3 = head2;
+            while (head3 != null) {
+                if (head1 == head3) {
+                    return head1.data;
+                }
+                head3 = head3.next;
+            }
+            head1 = head1.next;
+        }
+        return 0;
+    }
+}
---
+++ src/main/java/com/hackerrank/linkedlist/TailNodeLL.java
@@ -0,0 +1,49 @@
+package com.hackerrank.linkedlist;
+
+public class TailNodeLL {
+    static class SinglyLinkedListNode {
+        int data;
+        SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int data) {
+            this.data = data;
+        }
+    }
+//*****
+    static int getNode(SinglyLinkedListNode head, int positionFromTail) {
+
+        SinglyLinkedListNode current = head;
+        SinglyLinkedListNode result = head;
+        long index = 0;
+        while(current!=null)
+        {
+            current=current.next;
+
+
+            if (index++>positionFromTail)
+            {
+                result=result.next;
+            }
+        }
+        return result.data;
+
+
+
+
+
+
+
+//
+//        if (head == null) {
+//            return positionFromTail-=-1;
+//        }
+//
+//        int b = getNode(head.next, positionFromTail);
+//        if (positionFromTail == 1)
+//            b=  head.data;
+//
+//
+//        return b;
+
+    }
+}
