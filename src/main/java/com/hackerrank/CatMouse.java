1546a53562f0eca5119316696ec7daf96e854139
EXTMASZL <deshmukh259.2010@gmail.com>
2020-01-27 23:03:27 +0000

    hackerrank

--- src/main/java/com/hackerrank/CatMouse.java
+++ src/main/java/com/hackerrank/CatMouse.java
@@ -1,4 +1,16 @@
 package com.hackerrank;
 
 public class CatMouse {
+
+    static String catAndMouse(int x, int y, int z) {
+
+        if (Math.abs(x - z) < Math.abs(y - z))
+            return ("Cat A");
+        else if (Math.abs(x - z) > Math.abs(y - z))
+            return ("Cat B");
+        else
+            return "Mouse C";
+
+
+    }
 }
