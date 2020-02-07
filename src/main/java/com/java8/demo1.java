3600b8067b26d4e9cfe503f9601bd3e16166576f
EXTMASZL <deshmukh259.2010@gmail.com>
2020-02-07 19:48:49 +0000

    hackerrank

--- src/main/java/com/java8/demo1.java
+++ src/main/java/com/java8/demo1.java
@@ -1,5 +1,9 @@
 package com.java8;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 interface I1 {
     int f1(int i);
 }
@@ -16,12 +20,20 @@ interface I1 {
 
 public class demo1 {
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         I1 i = e -> e + 100;
         int v = i.f1(448);
         System.out.println(v);
 
     }
 
+    public static void main(String[] args) {
+        List<Integer> l = Arrays.asList(1,2,3,4);
+        l.forEach(System.out::println);
+        List<Integer> v= new ArrayList<>();
+
+        System.out.println(l.stream().reduce(0,Integer::sum));
+
+    }
 }
 
