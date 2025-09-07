5dff1370423a8aeb600ff550b995efe29b8403ff
purushottam <deshmukh259.2010@gmail.com>
2025-09-07 19:12:49 +0530

    supplier

--- src/main/java/com/lambda/SuppliesrDe.java
+++ src/main/java/com/lambda/SuppliesrDe.java
@@ -1,4 +1,29 @@
 package com.lambda;
 
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 public class SuppliesrDe {
+
+    public static void main(String[] args) {
+        Supplier<StringBuilder> sb = () -> new StringBuilder();
+
+        System.out.println(sb.get().append("Abcd"));
+
+        Consumer<StringBuilder> cc = v -> System.out.println(v);
+
+        cc.accept(sb.get().append("Ancd"));
+
+
+        List<String> v = List.of("Abcd","zxccc");
+
+        Consumer<String> cons1 = c-> System.out.println("c = " + c);
+
+        v.forEach(cons1);
+
+
+
+    }
+
 }
