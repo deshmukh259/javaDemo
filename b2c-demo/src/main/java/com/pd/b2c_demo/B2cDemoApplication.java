a58cfa8baf6c2a0011dc64673faa5325ad1e1cfc
purushottam <deshmukh259.2010@gmail.com>
2026-03-09 23:55:22 +0530

    b2c step 1:
    create app
    simple html page render

---
+++ b2c-demo/src/main/java/com/pd/b2c_demo/B2cDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pd.b2c_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2cDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2cDemoApplication.class, args);
+	}
+
+}
---
+++ b2c-demo/src/test/java/com/pd/b2c_demo/B2cDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pd.b2c_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2cDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
